0b2166b1eb3445261f3712c92d7b1c155b80690e
unknown <currently.unkwn@gmail.com>
2022-11-30 20:39:50 +0200

    Add static tweet components

--- src/index.js
+++ src/index.js
@@ -3,9 +3,67 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
-function Tweet() {
-  return <div className="tweet">Tweet</div>;
-}
+const Avatar = () => {
+  return (
+    <img
+      src="https://www.gravatar.com/avatar/nothing"
+      className="avatar"
+      alt="avatar"
+    />
+  );
+};
+
+const Message = () => {
+  return <div className="message">This is less than 40 characters</div>;
+};
+
+const NameWithHandle = () => {
+  return (
+    <span className="name-with-handle">
+      <span className="name">Your name</span>
+      <span className="handle">@yourhandle</span>
+    </span>
+  );
+};
+
+const Time = () => {
+  return <span className="time">3h ago</span>;
+};
+
+const ReplyButton = () => {
+  return <i className="fa fa-reply reply-button" />;
+};
+
+const RetweetButton = () => {
+  return <i className="fa fa-retweet retweet-button" />;
+};
+
+const LikeButton = () => {
+  return <i className="fa fa-heart like-button" />;
+};
+
+const MoreOptionsButton = () => {
+  return <i className="fa fa-ellipsis-h more-options-button" />;
+};
+
+const Tweet = () => {
+  return (
+    <div className="tweet">
+      <Avatar />
+      <div className="content">
+        <NameWithHandle />
+        <Time />
+        <Message />
+        <div className="buttons">
+          <ReplyButton />
+          <RetweetButton />
+          <LikeButton />
+          <MoreOptionsButton />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Tweet />);
